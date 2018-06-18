1db420ceaed29269cf9dd5e68fdfeb29ded66caf
Marten Prieß <marten@rocketbase.io>
2018-06-18 23:20:26 +0200

    replaced hibernate validation by javax

--- commons-auth-api/src/main/java/io/rocketbase/commons/dto/forgot/ForgotPasswordRequest.java
+++ commons-auth-api/src/main/java/io/rocketbase/commons/dto/forgot/ForgotPasswordRequest.java
@@ -4,8 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Email;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
