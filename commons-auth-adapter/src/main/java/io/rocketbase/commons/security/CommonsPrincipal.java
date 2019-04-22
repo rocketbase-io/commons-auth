bd488a3d9abbc57fb164c85d183f3d5f5bcb3afd
Marten <marten@rocketbase.io>
2019-04-22 21:51:13 +0200

    add shorthand to get current commonsPrincipal

--- commons-auth-adapter/src/main/java/io/rocketbase/commons/security/CommonsPrincipal.java
+++ commons-auth-adapter/src/main/java/io/rocketbase/commons/security/CommonsPrincipal.java
@@ -2,6 +2,8 @@ package io.rocketbase.commons.security;
 
 import io.rocketbase.commons.dto.appuser.AppUserRead;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.security.Principal;
 
@@ -11,6 +13,18 @@ public class CommonsPrincipal extends AppUserRead implements Principal {
     public CommonsPrincipal(AppUserRead user) {
         super(user.getId(), user.getUsername(), user.getFirstName(), user.getLastName(), user.getEmail(),
                 user.getAvatar(), user.getRoles(), user.getKeyValues(), user.getEnabled(), user.getCreated(), user.getLastLogin());
+    }
+
+    /**
+     * @return the <code>CommonsPrincipal</code> or <code>null</code> if no authentication
+     * * information is available
+     */
+    public static CommonsPrincipal getCurrent() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication instanceof CommonsAuthenticationToken) {
+            return ((CommonsAuthenticationToken) authentication).getPrincipal();
+        }
+        return null;
     }
 
     @Override
