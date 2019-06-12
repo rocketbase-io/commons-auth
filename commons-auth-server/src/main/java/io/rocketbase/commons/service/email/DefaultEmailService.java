1d37174b3bfa98c429671252129bbc3b8351a2a3
Marten <marten@rocketbase.io>
2019-06-12 23:26:45 +0200

    lowered JavaMailSender required. Now optional and fallback to log

--- commons-auth-server/src/main/java/io/rocketbase/commons/service/email/DefaultEmailService.java
+++ commons-auth-server/src/main/java/io/rocketbase/commons/service/email/DefaultEmailService.java
@@ -5,14 +5,19 @@ import io.rocketbase.commons.model.AppUser;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.nio.charset.StandardCharsets;
 
+@Slf4j
 @RequiredArgsConstructor
 public class DefaultEmailService implements EmailService {
 
@@ -20,10 +25,21 @@ public class DefaultEmailService implements EmailService {
     final InternetAddress from;
 
     @Resource
-    private JavaMailSender emailSender;
+    private ApplicationContext applicationContext;
 
     @Resource
     private MailContentConfig mailContentConfig;
+
+    private JavaMailSender emailSender;
+
+    @PostConstruct
+    public void init() {
+        try {
+            emailSender = applicationContext.getBean(JavaMailSender.class);
+        } catch (BeansException e) {
+            log.error("JavaMailSender is missing. Mails will only get logged. For production please configure spring-boot-starter-mail.");
+        }
+    }
 
     @Override
     @SneakyThrows
@@ -49,15 +65,19 @@ public class DefaultEmailService implements EmailService {
     @Override
     @SneakyThrows
     public void sentEmail(InternetAddress to, String subject, HtmlTextEmail htmlTextEmail, InternetAddress from) {
-        MimeMessage message = emailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message,
-                MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
-                StandardCharsets.UTF_8.name());
-        helper.setTo(to);
-        helper.setSubject(subject);
-        helper.setText(htmlTextEmail.getText(), htmlTextEmail.getHtml());
-        helper.setFrom(from);
-        emailSender.send(message);
+        if (emailSender == null) {
+            log.error("JavaMailSender is missing. SentEmail will only get logged.\nsentEmail: {} to: {} message:\n", subject, to.getAddress(), htmlTextEmail.getText());
+        } else {
+            MimeMessage message = emailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message,
+                    MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                    StandardCharsets.UTF_8.name());
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(htmlTextEmail.getText(), htmlTextEmail.getHtml());
+            helper.setFrom(from);
+            emailSender.send(message);
+        }
     }
 
 }
