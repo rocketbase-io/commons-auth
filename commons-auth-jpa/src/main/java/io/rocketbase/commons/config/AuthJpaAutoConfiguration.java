4809d29a54b8531ac1bda845a25fddd1bd877548
Marten <marten@rocketbase.io>
2019-08-30 12:28:25 +0200

    fixed autoconfiguration for jpa

--- commons-auth-jpa/src/main/java/io/rocketbase/commons/config/AuthJpaAutoConfiguration.java
+++ commons-auth-jpa/src/main/java/io/rocketbase/commons/config/AuthJpaAutoConfiguration.java
@@ -1,19 +1,22 @@
 package io.rocketbase.commons.config;
 
+import io.rocketbase.commons.model.AppUserJpaEntity;
 import io.rocketbase.commons.repository.AppUserJpaRepository;
 import io.rocketbase.commons.service.AppUserJpaServiceImpl;
 import io.rocketbase.commons.service.AppUserPersistenceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@AutoConfigureBefore(AuthServerAutoConfiguration.class)
 public class AuthJpaAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    public AppUserPersistenceService appUserPersistenceService(@Autowired AppUserJpaRepository appUserJpaRepository) {
+    public AppUserPersistenceService<AppUserJpaEntity> appUserPersistenceService(@Autowired AppUserJpaRepository appUserJpaRepository) {
         return new AppUserJpaServiceImpl(appUserJpaRepository);
     }
 }
--- commons-auth-mongo/src/main/java/io/rocketbase/commons/config/AuthMongoAutoConfiguration.java
+++ commons-auth-mongo/src/main/java/io/rocketbase/commons/config/AuthMongoAutoConfiguration.java
@@ -4,12 +4,14 @@ import io.rocketbase.commons.model.AppUserMongoEntity;
 import io.rocketbase.commons.service.AppUserMongoServiceImpl;
 import io.rocketbase.commons.service.AppUserPersistenceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
+@AutoConfigureBefore(AuthServerAutoConfiguration.class)
 public class AuthMongoAutoConfiguration {
 
     @Bean
