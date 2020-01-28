5d929211c8a47c44a8d63139eee6638f01df85e7
Marten <marten@rocketbase.io>
2020-01-28 20:05:39 +0100

    fix: missing property invitor

--- commons-auth-service/src/main/java/io/rocketbase/commons/service/email/SimpleMailContentConfig.java
+++ commons-auth-service/src/main/java/io/rocketbase/commons/service/email/SimpleMailContentConfig.java
@@ -83,7 +83,7 @@ public class SimpleMailContentConfig implements MailContentConfig {
                 .addButton(messageSource.getMessage("auth.email.invite.button", new Object[]{}, currentLocale), actionUrl).color(new ColorStyle("fff", "7FA162")).center().and()
                 .addText(String.format("- %s", emailProperties.getServiceName())).and()
                 .addFooter(String.format("%s<br>%s",
-                        messageSource.getMessage("auth.email.invite.footer.youReceiveThisEmailBecause", new Object[]{emailProperties.getServiceName()}, currentLocale),
+                        messageSource.getMessage("auth.email.invite.footer.youReceiveThisEmailBecause", new Object[]{invite.getInvitor(), emailProperties.getServiceName()}, currentLocale),
                         messageSource.getMessage("auth.email.invite.footer.ifYouAreNotSureWhy", new Object[]{emailProperties.getSupportEmail()}, currentLocale))).and()
                 .copyright(emailProperties.getCopyrightName()).url(emailProperties.getCopyrightUrl())
                 .build();
