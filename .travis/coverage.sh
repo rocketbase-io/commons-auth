cf921cefe227174bf221636556032a07ffb5341f
Marten <marten@rocketbase.io>
2021-02-17 09:36:22 +0100

    ci: fixed script

--- .travis/coverage.sh
+++ .travis/coverage.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-sed -e 's/<[\/]\?group[^>]*>//g' ./coverage/target/site/jacoco-aggregate/jacoco.xml > ./coverage/site/jacoco-aggregate/jacoco-cc.xml
-JACOCO_SOURCE_PATH="commons-auth-adapter/src/main/java commons-auth-api/src/main/java commons-auth-core/src/main/java commons-auth-email-postmark/src/main/java commons-auth-email-smtp/src/main/java commons-auth-forms/src/main/java commons-auth-jpa/src/main/java commons-auth-jpa/target/generated-sources/annotations commons-auth-mongo/src/main/java commons-auth-server/src/main/java commons-auth-service/src/main/java commons-auth-test/src/main/java" ./cc-test-reporter format-coverage -t jacoco ./coverage/target/site/jacoco-aggregate/jacoco-cc.xml
+sed -e 's/<[\/]\?group[^>]*>//g' coverage/target/site/jacoco-aggregate/jacoco.xml > coverage/target/site/jacoco-aggregate/jacoco-cc.xml
+
+export JACOCO_SOURCE_PATH=$(find . -type d -regex '.*main/java' | xargs echo)
+export JACOCO_SOURCE_PATH="$JACOCO_SOURCE_PATH $(find . -type d -regex '.*target/generated-sources/annotations' | xargs echo)"
+
+./cc-test-reporter format-coverage -t jacoco ./coverage/target/site/jacoco-aggregate/jacoco-cc.xml
 
 ./cc-test-reporter upload-coverage
